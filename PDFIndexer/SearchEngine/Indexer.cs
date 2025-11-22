0dcf966d0d63d17023925db783ec8239dd6ceaba
Hyeong-Jong Choi <0x@686a.io>
2025-11-23 00:05:56 +0900

    Refactor: Group files into dedicated folders

---
+++ PDFIndexer/SearchEngine/Indexer.cs
@@ -0,0 +1,140 @@
+﻿using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using PDFIndexer.Journal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+
+namespace PDFIndexer.SearchEngine
+{
+    internal class Indexer
+    {
+        private LuceneProvider Provider;
+
+        public Indexer(LuceneProvider provider)
+        {
+            Provider = provider;
+        }
+
+        /// <summary>
+        /// 주어진 PDF 파일들을 인덱싱합니다.
+        /// </summary>
+        /// <param name="pdfs">인덱싱할 pdf들의 경로입니다</param>
+        public void IndexPdfs(string[] pdfs)
+        {
+            Logger.Write(JournalLevel.Info, $"IndexPdfs 요청 - 요청된 PDF 수:{pdfs.Length}");
+
+            var writer = Provider.GetIndexWriter();
+
+            foreach (var path in pdfs)
+            {
+                var filename = (path.Split('\\').LastOrDefault() ?? path).Replace(".pdf", "");
+
+                var hash = IOUtil.GetHashFromFile(path);
+                var lastModified = IOUtil.GetLastModifiedFromFile(path).ToString();
+
+                using (PdfDocument pdf = PdfDocument.Open(path))
+                {
+                    // 페이지 별로 인덱스
+                    var pages = pdf.GetPages();
+                    foreach (var page in pages)
+                    {
+                        // 기본 메타데이터
+                        Document doc = new Document
+                        {
+                            new TextField("title", filename, Field.Store.YES),
+                            new StringField("path", path, Field.Store.YES),
+                            new Int32Field("page", page.Number, Field.Store.YES),
+                            new StringField("md5", hash, Field.Store.YES),
+                            new StringField("lastModified", lastModified, Field.Store.YES),
+                        };
+
+                        // 페이지 내용 (텍스트만)
+                        string content = ContentOrderTextExtractor.GetText(page);
+                        doc.Add(new TextField("content", content, Field.Store.YES));
+
+                        // TODO: Cron task에 이미지 OCR
+
+                        writer.AddDocument(doc);
+                    }
+
+                    // Logger.Write($"IndexPdfs - Index done: {path} with {pages.Count()} pages");
+                }
+            }
+
+            Logger.Write(JournalLevel.Info, "IndexPdfs 정리 중..");
+            writer.Commit();
+            writer.Dispose();
+
+            Logger.Write(JournalLevel.Info, "IndexPdfs 완료.");
+
+            Provider.Initialize();
+        }
+
+        public void CleanupIndexes()
+        {
+            // 인덱스를 업데이트하기 위한 reader/writer 가져오기
+            var writer = Provider.GetIndexWriter();
+            var reader = Provider.GetIndexReader();
+
+            // 업데이트가 필요한 문서
+            List<string> toUpdate = new List<string>();
+
+            // 인덱스를 구성하는 모든 리프 순회
+            foreach (var leafContext in reader.Leaves)
+            {
+                var leadReader = leafContext.Reader;
+
+                // 삭제되지 않은 문서 정보 가져오기
+                var liveDocs = MultiFields.GetLiveDocs(leadReader);
+
+                // 모든 문서 순회
+                for (int id = 0; id < leadReader.MaxDoc; id++)
+                {
+                    // 삭제된 문서 스킵
+                    if (liveDocs != null && !liveDocs.Get(id)) continue;
+
+                    Document document = leadReader.Document(id);
+
+                    string path = document.Get("path");
+                    if (!File.Exists(path))
+                    {
+                        writer.DeleteDocuments(new Term("path", path));
+                        continue;
+                    }
+
+                    // 해시 구하는 건 CPU 시간을 많이 사용하니, cron task에서는 미사용.
+                    // hash로 비교는 toIndexingFiles < indexedFiles 일 때만 사용.
+                    //
+                    // 해당 path에는 존재하는데 hash가 다르면
+                    // 파일이 업데이트/변경된거 --> 새로 인덱싱 해야함
+                    // string indexedHash = document.Get("hash");
+                    // string realHash = GetHashFromFile(path);
+
+                    // 인덱스의 last modified와 실제 last modified의 시간이 다르면
+                    // 파일이 변경되었으므로, 새로 인덱싱 해야 함
+                    long lastModifiedFromIndex = Int64.Parse(document.Get("lastModified"));
+                    long lastModifiedFromFile = IOUtil.GetLastModifiedFromFile(path);
+                    if (lastModifiedFromIndex != lastModifiedFromFile)
+                    {
+                        toUpdate.Add(path);
+                    }
+                }
+            }
+
+            // 삭제 완료 --> 인덱스 저장
+            writer.Commit();
+            writer.Dispose();
+
+            Provider.Initialize();
+
+            // 업데이트가 필요한 문서들 업데이트
+            //indexer.IndexPdfs(toUpdate.ToArray());
+        }
+    }
+}
---
+++ PDFIndexer/SearchEngine/LuceneProvider.cs
@@ -0,0 +1,97 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Analysis.Cjk;
+using Lucene.Net.Documents;
+using Lucene.Net.Index;
+using Lucene.Net.Search;
+using Lucene.Net.Store;
+using Lucene.Net.Util;
+using PDFIndexer.Journal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuceneDirectory = Lucene.Net.Store.Directory;
+
+namespace PDFIndexer.SearchEngine
+{
+    internal class LuceneProvider
+    {
+        private string BasePath;
+
+        private const LuceneVersion luceneVersion = LuceneVersion.LUCENE_48;
+
+        private LuceneDirectory IndexDirectory;
+        private Analyzer analyzer;
+        private IndexReader reader;
+        private IndexSearcher searcher;
+
+        public LuceneProvider(string basePath)
+        {
+            BasePath = basePath;
+
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            Logger.Write(JournalLevel.Info, "LuceneProvider 초기화");
+
+            IndexDirectory = FSDirectory.Open(Path.Combine(BasePath, ".index"));
+            analyzer = new CJKAnalyzer(luceneVersion);
+
+            try
+            {
+                searcher = InitializeIndexSearcher();
+                // SetReadyState(true);
+
+                Logger.Write("기존 인덱스 찾음");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                // 인덱스 없음
+                searcher = null;
+
+                Logger.Write(JournalLevel.Warning, "디스크에 저장된 인덱스 없음");
+            }
+
+            Logger.Write(JournalLevel.Info, "LuceneProvider 초기화 --> 완료");
+        }
+
+        private IndexSearcher InitializeIndexSearcher()
+        {
+            reader = DirectoryReader.Open(IndexDirectory);
+            return new IndexSearcher(reader);
+        }
+
+        public IndexWriter GetIndexWriter()
+        {
+            IndexWriterConfig indexConfig = new IndexWriterConfig(luceneVersion, analyzer);
+            indexConfig.OpenMode = OpenMode.CREATE;
+            IndexWriter writer = new IndexWriter(IndexDirectory, indexConfig);
+
+            return writer;
+        }
+
+        public IndexReader GetIndexReader()
+        {
+            return reader;
+        }
+
+        public Analyzer GetAnalyzer()
+        {
+            return analyzer;
+        }
+
+        public IndexSearcher GetIndexSearcher()
+        {
+            return searcher;
+        }
+
+        public Document SearchDocument(int doc)
+        {
+            return searcher.Doc(doc);
+        }
+    }
+}
