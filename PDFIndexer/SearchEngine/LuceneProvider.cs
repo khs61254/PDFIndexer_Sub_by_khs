b4c765ced942982163d4fdba739deb80c7b2e88f
Hyeong-Jong Choi <0x@686a.io>
2025-11-24 19:50:30 +0900

    Add ready event and dispose methods to lucene provider

--- PDFIndexer/SearchEngine/LuceneProvider.cs
+++ PDFIndexer/SearchEngine/LuceneProvider.cs
@@ -16,34 +16,85 @@ using LuceneDirectory = Lucene.Net.Store.Directory;
 
 namespace PDFIndexer.SearchEngine
 {
-    internal class LuceneProvider
+    public class LuceneProvider : IDisposable
     {
         private string BasePath;
 
         private const LuceneVersion luceneVersion = LuceneVersion.LUCENE_48;
 
-        private LuceneDirectory IndexDirectory;
-        private Analyzer analyzer;
-        private IndexReader reader;
-        private IndexSearcher searcher;
+        private static LuceneDirectory IndexDirectory;
+        private static Analyzer Analyzer;
+        private static IndexReader Reader;
+        private static IndexSearcher Searcher;
+
+        private bool _IsDisposed;
+
+        private static bool _Ready = false;
+        public static bool Ready { get { return _Ready; } }
+
+        /// <summary>
+        /// Indexer가 준비될때 발생합니다. 이벤트가 Main Thread에서 발생하지 않을 수도 있습니다.
+        /// </summary>
+        public static event ReadyEventHandler OnReady;
+
+        public delegate void ReadyEventHandler();
 
         public LuceneProvider(string basePath)
         {
             BasePath = basePath;
+        }
 
-            Initialize();
+        ~LuceneProvider()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_IsDisposed) return;
+
+            SetReadyState(false);
+
+            if (disposing)
+            {
+                if (IndexDirectory != null) IndexDirectory.Dispose();
+                if (Analyzer != null) Analyzer.Dispose();
+                if (Reader != null) Reader.Dispose();
+            }
+
+            _IsDisposed = true;
+        }
+
+        private void SetReadyState(bool ready)
+        {
+            _Ready = ready;
+
+            if (ready) DispatchReadyEvent();
+        }
+
+        private void DispatchReadyEvent()
+        {
+            Logger.Write(JournalLevel.Info, "Indexer Ready");
+            OnReady?.Invoke();
         }
 
         public void Initialize()
         {
             Logger.Write(JournalLevel.Info, "LuceneProvider 초기화");
+            SetReadyState(false);
 
             IndexDirectory = FSDirectory.Open(Path.Combine(BasePath, ".index"));
-            analyzer = new CJKAnalyzer(luceneVersion);
+            Analyzer = new CJKAnalyzer(luceneVersion);
 
             try
             {
-                searcher = InitializeIndexSearcher();
+                Searcher = InitializeIndexSearcher();
                 // SetReadyState(true);
 
                 Logger.Write("기존 인덱스 찾음");
@@ -51,23 +102,25 @@ namespace PDFIndexer.SearchEngine
             catch (DirectoryNotFoundException)
             {
                 // 인덱스 없음
-                searcher = null;
+                Searcher = null;
 
                 Logger.Write(JournalLevel.Warning, "디스크에 저장된 인덱스 없음");
             }
 
             Logger.Write(JournalLevel.Info, "LuceneProvider 초기화 --> 완료");
+
+            SetReadyState(true);
         }
 
         private IndexSearcher InitializeIndexSearcher()
         {
-            reader = DirectoryReader.Open(IndexDirectory);
-            return new IndexSearcher(reader);
+            Reader = DirectoryReader.Open(IndexDirectory);
+            return new IndexSearcher(Reader);
         }
 
         public IndexWriter GetIndexWriter()
         {
-            IndexWriterConfig indexConfig = new IndexWriterConfig(luceneVersion, analyzer);
+            IndexWriterConfig indexConfig = new IndexWriterConfig(luceneVersion, Analyzer);
             indexConfig.OpenMode = OpenMode.CREATE;
             IndexWriter writer = new IndexWriter(IndexDirectory, indexConfig);
 
@@ -76,22 +129,22 @@ namespace PDFIndexer.SearchEngine
 
         public IndexReader GetIndexReader()
         {
-            return reader;
+            return Reader;
         }
 
         public Analyzer GetAnalyzer()
         {
-            return analyzer;
+            return Analyzer;
         }
 
         public IndexSearcher GetIndexSearcher()
         {
-            return searcher;
+            return Searcher;
         }
 
         public Document SearchDocument(int doc)
         {
-            return searcher.Doc(doc);
+            return Searcher.Doc(doc);
         }
     }
 }
