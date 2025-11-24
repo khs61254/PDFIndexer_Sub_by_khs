3855e82e7c976a2c3ad03c8b24c06468c8d694b9
Hyeong-Jong Choi <0x@686a.io>
2025-11-24 19:53:38 +0900

    Update code for new search engine system

--- PDFIndexer/Program.cs
+++ PDFIndexer/Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using PDFIndexer.Journal;
+using PDFIndexer.SearchEngine;
 using PDFIndexer.SetupWizard;
 
 namespace PDFIndexer
 {
     internal static class Program
     {
-        private static readonly Properties.Settings AppSettiongs = Properties.Settings.Default;
+        private static readonly Properties.Settings AppSettings = Properties.Settings.Default;
 
         /// <summary>
         /// The main entry point for the application.
@@ -32,28 +34,33 @@ namespace PDFIndexer
             Application.SetCompatibleTextRenderingDefault(false);
 
             // 초기 실행 시 Setup wizard 실행
-            if (!AppSettiongs.DoneSetupWizard)
+            if (!AppSettings.DoneSetupWizard)
             {
                 Logger.Write($"Settings.DoneSetupWizard : false --> Setup wizard 실행");
                 Application.Run(new SetupWizardForm());
+
+                return;
             }
-            else
-            {
-                Logger.Write($"메인 UI 실행");
-                Application.Run(new Form1());
-            }
+
+            LuceneProvider luceneProvider = new LuceneProvider(AppSettings.BasePath);
+            luceneProvider.Initialize();
+
+            Logger.Write($"메인 UI 실행");
+            Application.Run(new Form1(luceneProvider));
+
+            luceneProvider.Dispose();
         }
 
         // 설정 기본값 저장
         private static void SetupDefaultSettings()
         {
-            if (string.IsNullOrWhiteSpace(AppSettiongs.BasePath))
+            if (string.IsNullOrWhiteSpace(AppSettings.BasePath))
             {
-                Logger.Write($"BasePath가 비어있음 : {AppSettiongs.BasePath}");
+                Logger.Write($"BasePath가 비어있음 : {AppSettings.BasePath}");
 
                 var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                AppSettiongs.BasePath = path;
-                AppSettiongs.Save();
+                AppSettings.BasePath = path;
+                AppSettings.Save();
 
                 Logger.Write($"BasePath 기본값 설정 : {path}");
             }
