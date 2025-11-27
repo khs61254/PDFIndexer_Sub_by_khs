6e5723c7fa76737f4aa8cb448b63f0d8f2fd47f5
Hyeong-Jong Choi <0x@686a.io>
2025-11-28 01:00:04 +0900

    Add icon

--- PDFIndexer/Properties/Resources.Designer.cs
+++ PDFIndexer/Properties/Resources.Designer.cs
@@ -131,6 +131,26 @@ namespace PDFIndexer.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon PDFIndexerIcon {
+            get {
+                object obj = ResourceManager.GetObject("PDFIndexerIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon PDFIndexerIcon_Tiny {
+            get {
+                object obj = ResourceManager.GetObject("PDFIndexerIcon-Tiny", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap SearchIcon {
